11c96dda338fd199ad076885fc311709135f76c8
nihonor <niyitanganihonor@gmail.com>
2025-04-25 14:10:29 +0200

    Implement issue listing in IssuePage with data fetched from the database and display in a responsive table format

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
-const IssuePage = () => {
+import prisma from "@/prisma/client";
+const IssuePage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
-    <div>
+    <div className="m-5">
       <Button>
         {" "}
         <Link href="/issues/new"> New issue</Link>
       </Button>
+      <Table.Root variant="surface" mt="5">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
