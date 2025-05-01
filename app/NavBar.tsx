aedf53c3ba8e79423b2e08cb18de13b6c41ac572
nihonor <niyitanganihonor@gmail.com>
2025-05-01 22:01:40 +0200

    Refactor NavBar to enhance user experience with a responsive layout; integrate Avatar and DropdownMenu for user authentication status display, replacing previous login/logout links with a more interactive design.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,14 @@ import { AiFillBug } from "react-icons/ai";
 import classnames from "classnames";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 
 const NavBar = () => {
   const links = [
@@ -13,34 +20,62 @@ const NavBar = () => {
     { label: "Issues", href: "/issues/list" },
   ];
   const currentPath = usePathname();
-  const{status,data:session}=useSession()
+  const { status, data: session } = useSession();
 
   return (
-    <nav className="flex space-x-6 mb-3 border py-6 px-6 items-center">
-      <Link href="/">
-        <AiFillBug size={24} />
-      </Link>
-      <ul className="flex space-x-6 text-zinc-500 ">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classnames({
-                "text-zinc-900": link.href === currentPath,
-                "text-zinc-500": link.href !== currentPath,
-                "hover:text-zinc-900 transition-colors": true,
-              })}
-              //   className="hover:text-zinc-800 transition-colors"
-            >
-              {link.label}
+    <nav className="mb-3 border py-6 px-6">
+      <Container>
+        <Flex justify="between">
+          <Flex align="center" gap="5">
+            <Link href="/">
+              <AiFillBug size={24} />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box>
-        {status==='authenticated'&&<Link href='/api/auth/signout'>Log out</Link>}
-        {status==='unauthenticated'&&<Link href='/api/auth/signin' >Log in</Link>}
-      </Box>
+            <ul className="flex space-x-6 text-zinc-500 ">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={classnames({
+                      "text-zinc-900": link.href === currentPath,
+                      "text-zinc-500": link.href !== currentPath,
+                      "hover:text-zinc-900 transition-colors": true,
+                    })}
+                    //   className="hover:text-zinc-800 transition-colors"
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "authenticated" && (
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user?.image!}
+                    fallback="?"
+                    size="3"
+                    radius="full"
+                    className="cursor-pointer"
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    <Text size="3">{session.user?.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href="/api/auth/signout">Log out</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Log in</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
