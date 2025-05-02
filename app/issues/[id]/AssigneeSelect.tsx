14ee8eeca1a337274e066adc74e2f30e12f18832
nihonor <niyitanganihonor@gmail.com>
2025-05-02 11:14:21 +0200

    Fetch and display users in AssigneeSelect component; replace static options with dynamic user data from API for improved functionality.

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,29 @@
-"use client"
+"use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    getUser();
+  }, []);
   return (
     <Select.Root>
       <Select.Trigger placeholder="assignee.." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggessions</Select.Label>
-          <Select.Item value="1">Honore</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
